f99ef0b2321ede97c6f3391a563ba3695c38d524
mailhyuil <mailhyuil@gmail.com>
2022-06-28 11:49:59 +0900

    update

---
+++ react-hello/public/array_data_01.js
@@ -0,0 +1,8 @@
+const getArray = () => {
+  return ["우리나라", "Korea"];
+};
+
+const [nation1, nation2] = getArray();
+
+console.log(nation1);
+console.log(nation2);
---
+++ react-hello/public/json_data_01.js
@@ -0,0 +1,10 @@
+const student = {
+  st_num: 1,
+  st_name: "홍길동",
+  st_grade: 3,
+  st_dept: "컴공과",
+};
+
+console.table(student);
+
+const { st_name, st_dept } = student;
