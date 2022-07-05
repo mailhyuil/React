b84657d5618f84b8b7af293ba253713721de243d
mailhyuil <mailhyuil@gmail.com>
2022-07-05 11:16:59 +0900

    update

---
+++ node-100-hello/config/mongoConfig.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const USERNAME = "hyuil";
+const PASSWORD = "12341234"
+const mongoURL = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.n6lttyl.mongodb.net/?retryWrites=true&w=majority`;
+
+/*
+mongoose 도구를 사용하여
+NoSQL인 mongoDB에 DMBS Schema 방식으로 접근하기
+*/
+const Schema = mongoose.Schema;
+
+const bucket = Schema({
+    b_id: String,
+    b_flag: Number,
+    b_sdate: String,
+    b_bucket: String,
+    b_edate: String,
+    b_cancel: Boolean,
+})
+
+
+
+const bucket_model = mongoose.model("bucket", bucket);
+export { mongoURL, bucket_model };
